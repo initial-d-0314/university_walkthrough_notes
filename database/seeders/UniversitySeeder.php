82d52dcba6e2476f4d3a0887bba4f83efdeabbad
initial-d-0314 <daidai20000314@gmail.com>
2023-10-02 11:58:14 +0900

    change seeder for deploy

--- database/seeders/UniversitySeeder.php
+++ database/seeders/UniversitySeeder.php
@@ -24,18 +24,4 @@ class UniversitySeeder extends Seeder
                 'created_at' => new DateTime(),
                 'updated_at' => new DateTime(),
         ]);
-        DB::table('universities')->insert([
-                'name' => '早稲田大学（早稲田キャンパス）',
-                'nearest_station' => '東京メトロ東西線　早稲田駅',
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime(),
-        ]);
-        DB::table('universities')->insert([
-                'name' => '東京工業大学（大岡山キャンパス）',
-                'section' => '工学系',
-                'nearest_station' => '大井町線　大岡山駅',
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime(),
-        ]);
-    }
 }
